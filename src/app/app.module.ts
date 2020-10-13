3fbcc1da56958c4bb219042f81ccce6be571f145
wisman101 <lewiskimani18@gmail.com>
2020-10-13 18:27:59 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,18 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from "./product.service";
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes} from "@angular/router";
+
+const appRoutes:Routes = [
+  {
+    path: products,
+    component: ProductListComponent
+  },
+  {
+    path: product/:id,
+    component:ProductPageComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -19,6 +31,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
